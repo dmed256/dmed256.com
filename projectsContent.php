a46ddae5347d0978f32b8b7ccbdbe51f0a112643
dsm5 <dmed256@gmail.com>
2015-03-12 23:09:11 -0500

    [Project] Added OCCA images

--- projectsContent.php
+++ projectsContent.php
@@ -1,3 +1,5 @@
-<div class="feed" dividerText="OCCA">
-  <a href="http://libocca.org/" class="link">http://libocca.org/</a>
+<div class="feed">
+  <h4 class="ui dividing header"> OCCA </h4>
+  <img src="/images/projects/occa/occaFlow.png" style="display: block; height: 400px;"></img>
+  <a href="http://libocca.org" class="link">Website</a>
 </div>
