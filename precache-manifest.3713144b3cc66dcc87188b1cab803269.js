f4328965fcc968107ffeb265f3e36153d22b1e69
dmed256 <dmed256@gmail.com>
2019-08-26 04:01:44 -0500

    Updates

---
+++ precache-manifest.3713144b3cc66dcc87188b1cab803269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1640e39e582e8197453d92a5876af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e1c9d15f498844442a",
+    "url": "/static/css/main.d4f94531.chunk.css"
+  },
+  {
+    "revision": "b0a8996f8628f551d63b",
+    "url": "/static/js/2.6fd28106.chunk.js"
+  },
+  {
+    "revision": "33e1c9d15f498844442a",
+    "url": "/static/js/main.f89234de.chunk.js"
+  },
+  {
+    "revision": "293e57c3eb410897fe03",
+    "url": "/static/js/runtime~main.7bb12474.js"
+  }
+]);
