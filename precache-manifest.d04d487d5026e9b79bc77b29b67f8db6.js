5a66c7e9794bb20fe6de3defca4f1bedfa637c59
dmed256 <dmed256@gmail.com>
2019-09-15 19:16:22 -0500

    Updates

---
+++ precache-manifest.d04d487d5026e9b79bc77b29b67f8db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c875fdda575d2c16f4350b78d8f07a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c88f88a36d09700c817",
+    "url": "/static/css/main.d4f94531.chunk.css"
+  },
+  {
+    "revision": "80b3c4a2a3570094b2f0",
+    "url": "/static/js/2.de6a36a9.chunk.js"
+  },
+  {
+    "revision": "2c88f88a36d09700c817",
+    "url": "/static/js/main.293c8357.chunk.js"
+  },
+  {
+    "revision": "b5e5261bb1bffb56aa23",
+    "url": "/static/js/runtime~main.98b3c8f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d9aba20643ed018d4c25ec5ea19652b.js"
+  "/precache-manifest.d04d487d5026e9b79bc77b29b67f8db6.js"
 );
 
 self.addEventListener('message', (event) => {
