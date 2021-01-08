e4ea29035689bd9129ce8bd7e25b5129812bf50c
dmed256 <dmed256@gmail.com>
2021-01-07 19:46:58 -0500

    Updates

--- precache-manifest.891f1ff2f89d4bbaa817b181242629d0.js
+++ precache-manifest.891f1ff2f89d4bbaa817b181242629d0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3be3f8c1a326be6e8c2dff069ff60593",
+    "revision": "05172491c65ae552a8a1c829522509fc",
     "url": "/index.html"
   },
   {
-    "revision": "57b87a2cc7f2fe123759",
+    "revision": "281e2985918ff8bce05f",
     "url": "/static/css/main.0668a573.chunk.css"
   },
   {
-    "revision": "e7120e49cfb0462769b9",
-    "url": "/static/js/2.4ade5075.chunk.js"
+    "revision": "2f4f7ee33a2db221ab53",
+    "url": "/static/js/2.c79325a9.chunk.js"
   },
   {
-    "revision": "57b87a2cc7f2fe123759",
-    "url": "/static/js/main.e92c5ec4.chunk.js"
+    "revision": "281e2985918ff8bce05f",
+    "url": "/static/js/main.050c6e78.chunk.js"
   },
   {
     "revision": "b5e5261bb1bffb56aa23",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a980884bdfff7d9faa2e0e1150879fc.js"
+  "/precache-manifest.891f1ff2f89d4bbaa817b181242629d0.js"
 );
 
 self.addEventListener('message', (event) => {
