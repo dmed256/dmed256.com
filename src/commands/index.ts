52e8b81c81e19fab5ab1da13769c3db51ff4ebb6
dmed256 <dmed256@gmail.com>
2024-08-04 15:03:55 -0700

    remove reset

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,13 +2,11 @@ import type { Command } from '@/types';
 import { clear } from '@/commands/clear';
 import { help } from '@/commands/help';
 import { ls } from '@/commands/ls';
-import { reset } from '@/commands/reset';
 import { welcome } from '@/commands/welcome';
 
 export const commands: Record<string, Command> = {
   clear,
   help,
   // ls,
-  reset,
   welcome,
 };
